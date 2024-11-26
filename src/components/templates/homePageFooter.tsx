8554f41a11d9a7d2fd57732f3e43a310058e5d28
x-niroshan-x <niroshanjwm@gmail.com>
2024-11-26 21:42:28 +0530

    fix responsive issues in home screen

--- src/components/templates/homePageFooter.tsx
+++ src/components/templates/homePageFooter.tsx
@@ -3,16 +3,17 @@ import Button from "../uis/button";
 
 const HomePageFooter = () => {
   return (
-    <div className="flex items-center justify-center p-20">
-      <div className="w-1/2">
+    <div className="flex flex-col sm:flex-row items-center justify-center p-20">
+      <div className="w-full sm:1/2">
         <Image
+          className="mb-10 sm:mb-0"
           src={"/blott-logo.png"}
           alt="Blott Logo"
           height={56}
           width={500}
         />
       </div>
-      <div className="w-1/2 flex justify-center">
+      <div className="w-full sm:1/2 flex justify-center">
         <Button label="Ready" />
       </div>
     </div>
